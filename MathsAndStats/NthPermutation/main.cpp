d19d35e88b7999a52ed7d565688222af8c9248c4
Aniket Bhushan <aniket2701bhushan@gmail.com>
2019-11-18 00:37:26 +0530

    Implement find 'nth permutation'

--- MathsAndStats/NthPermutation/main.cpp
+++ MathsAndStats/NthPermutation/main.cpp
@@ -7,9 +7,38 @@
 //
 
 #include <iostream>
+#include <string>
+
+long long factorial(int num)
+{
+    return (num == 0 || num == 1) ? 1 : num*factorial(num-1);
+}
+
+// Assuming str is in sorted order
+std::string getKthPermutation(const std::string& str, int k)
+{
+    std::string dest{};
+    std::string source = str;
+    
+    long long f = factorial(str.size());
+    while (!source.empty())
+    {
+        f = f/source.size();
+        
+        dest.push_back(source[k/f]);
+        source.erase(source.begin()+k/f);
+        k = k%f;
+    }
+    return dest;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string str {"12345"};
+    
+    auto numPermutations = factorial(str.size());
+    for (int i=0; i<numPermutations; ++i) {
+        std::cout << getKthPermutation(str, i) << std::endl;
+    }
+    
     return 0;
 }
