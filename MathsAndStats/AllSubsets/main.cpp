de50c02b001774502c3d536f045411556fac7d44
Aniket Bhushan <aniket2701bhushan@gmail.com>
2019-11-17 22:43:47 +0530

    Added "math and stats"

---
+++ MathsAndStats/AllSubsets/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AllSubsets
+//
+//  Created by Aniket Bhushan on 17/11/19.
+//  Copyright © 2019 Aniket Bhushan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
