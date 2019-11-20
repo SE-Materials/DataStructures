dd2f2a39a72915d26dfc4f80c99e9f20c012e45e
Aniket Bhushan <aniket2701bhushan@gmail.com>
2019-11-20 23:44:53 +0530

    permutation of string

--- MathsAndStats/PermuteString/main.cpp
+++ MathsAndStats/PermuteString/main.cpp
@@ -7,9 +7,56 @@
 //
 
 #include <iostream>
+#include <string>
+#include <unordered_set>
+
+//std::ostream& operator<<(std::ostream& os, const std::vector<std::string>& other)
+//{
+//    for (auto& s : other)
+//        os << s << " ";
+//    return os;
+//}
+
+std::unordered_set<std::string> st;
+
+void recursive(std::string rem, std::string str)
+{
+    if (rem.empty())
+    {
+        auto iter = st.find(str);
+        if (iter == st.end())
+        {
+            st.insert(str);
+            std::cout << str << " ";
+        }
+    }
+    
+    for (int i=0; i<rem.size(); ++i)
+        recursive(rem.substr(0, i) + rem.substr(i+1), str + rem[i]);
+}
+
+void stlPermutation(std::string& str)
+{
+    do
+    {
+        std::cout << str <<  " ";
+    }
+    while (std::next_permutation(str.begin(), str.end()));
+}
+
+void printAllPermutations(const std::string& str)
+{
+    recursive(str, "");
+    std::cout << std::endl;
+    
+    std::string nstr = str;
+    stlPermutation(nstr);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string str("ABCA");
+    std::sort(str.begin(), str.end());
+    printAllPermutations(str);
+    
     return 0;
 }
