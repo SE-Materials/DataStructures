892ed9196533851597d6cb648a73bb2ae2e025a6
Aniket Bhushan <aniket2701bhushan@gmail.com>
2019-11-29 00:39:11 +0530

    square root and all subsets

--- MathsAndStats/AllSubsets/main.cpp
+++ MathsAndStats/AllSubsets/main.cpp
@@ -7,9 +7,45 @@
 //
 
 #include <iostream>
+#include <vector>
+
+std::ostream& operator<<(std::ostream& os, const std::vector<int>& vi)
+{
+    for (const auto& e : vi)
+        os << e << " ";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<int>>& vvi)
+{
+    for (const auto& v : vvi)
+        os << v << std::endl;
+    return os;
+}
+
+void findAllSubsets(std::vector<std::vector<int>>& sets, const std::vector<int>& v, int i, std::vector<int>& vc)
+{
+    if (i >= v.size()) {
+        sets.push_back(vc);
+        return;
+    }
+    
+    // Include ii
+    vc.push_back(v[i]);
+    findAllSubsets(sets, v, i+1, vc);
+
+    
+    // Don't include ii
+    vc.pop_back();
+    findAllSubsets(sets, v, i+1, vc);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<int> v {1, 2, 3, 4};
+    std::vector<std::vector<int>> ans;
+    std::vector<int> vi;
+    
+    findAllSubsets(ans, v, 0, vi);
+    std::cout << ans << std::endl;
     return 0;
 }
--- MathsAndStats/CalculateSquareRoot/main.cpp
+++ MathsAndStats/CalculateSquareRoot/main.cpp
@@ -8,8 +8,55 @@
 
 #include <iostream>
 
+double sqrt(int n, int precision)
+{
+    int l = 0, r = n;
+    float ans = 0;
+    while (l <= r)
+    {
+        int mid = (l + r)/2;
+        if (mid*mid == n)
+        {
+            ans = mid;
+            break;
+        }
+        else if (mid*mid > n)
+        {
+            r = mid-1;
+            ans = r;
+        }
+        else
+        {
+            l = mid+1;
+        }
+    }
+    
+    float incremnt = 0.1;
+    
+    for (int i=0; i<precision; ++i)
+    {
+        while (ans*ans <= n)
+        {
+            ans += incremnt;
+        }
+        
+        ans = ans - incremnt;
+        incremnt /= 10;
+    }
+
+    return ans;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    std::cout << sqrt(1, 2) << std::endl;
+    std::cout << sqrt(2, 2) << std::endl;
+    std::cout << sqrt(3, 2) << std::endl;
+    std::cout << sqrt(4, 2) << std::endl;
+    std::cout << sqrt(5, 2) << std::endl;
+    std::cout << sqrt(6, 2) << std::endl;
+    std::cout << sqrt(7, 2) << std::endl;
+    std::cout << sqrt(8, 2) << std::endl;
+    std::cout << sqrt(9, 2) << std::endl;
     return 0;
 }
