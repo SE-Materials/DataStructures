50b19a9ef0550be3a84b0bc67c29aab5f3fcff39
Aniket Bhushan <aniket11bh@users.noreply.github.com>
2022-04-21 00:37:55 +0530

    Create BST Iterative PreOrder.cpp

---
+++ Tree/BST Iterative PreOrder.cpp
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+        vector<int>      ans;
+        stack<TreeNode*> st;
+        
+        if (root != nullptr)
+            st.push(root);
+        
+        while (!st.empty())
+        {
+            root = st.top(); st.pop();
+            
+            if (root->right)
+                st.push(root->right);
+            
+            if (root->left)
+                st.push(root->left);
+            
+            ans.push_back(root->val);
+        }
+        
+        return ans;
+    }
+};
